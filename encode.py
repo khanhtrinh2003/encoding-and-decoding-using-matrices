58edc2be8245340d9177b460ab855f680537b84b
khanhtrinh2003 <nguyenduckhanhtrinh@gmail.com>
2019-03-13 20:37:18 +0700

    encode

---
+++ encode.py
@@ -0,0 +1,25 @@
+m = 0
+while m < 1:
+    key = {'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q', 'e': 'r', 'f': 's', 'g': 't', 'h': 'u',
+           'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y', 'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c',
+           'q': 'd', 'r': 'e', 's': 'f', 't': 'g', 'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k',
+           'y': 'l', 'z': 'm', 'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S',
+           'G': 'T', 'H': 'U', 'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z', 'N': 'A',
+           'O': 'B', 'P': 'C', 'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I',
+           'W': 'J', 'X': 'K', 'Y': 'L', 'Z': 'M'}
+    chuoi = input('\nType the sentence: ')
+
+    password = []
+
+    for i in range(len(chuoi)):
+        while True:
+            try:
+                x = key[chuoi[i]]
+                password.append(x)
+                break
+            except KeyError:
+                print('Type again!')
+                break
+    print('result: ',''.join(str(e) for e in password))
+    continue
+
